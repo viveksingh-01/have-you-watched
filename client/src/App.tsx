68b713a1696b4e0ae7c0accc4c29dced12c50910
viveksingh-01 <viveksingh9301@gmail.com>
2021-01-16 18:04:49 +0530

    Install react-bootstrap package and add navbar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import './../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import LandingPage from './pages/LandingPage';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Have You Watched?!</header>
+      <LandingPage />
     </div>
   );
 }
---
+++ client/src/pages/LandingPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import NavigationBar from '../components/NavigationBar';
+
+const LandingPage = () => {
+  return (
+    <>
+      <NavigationBar />
+    </>
+  );
+};
+
+export default LandingPage;
