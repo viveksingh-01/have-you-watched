e30657bc9adb28c490d83ed4da0900763ce008bc
viveksingh-01 <viveksingh9301@gmail.com>
2021-01-17 02:01:10 +0530

    Modify movie title and rating styles in MovieCard

--- client/src/components/MovieCard.tsx
+++ client/src/components/MovieCard.tsx
@@ -7,8 +7,11 @@ const MovieCard = ({ movie }: { movie: Movie }) => {
     <>
       <Card style={{ width: '18rem' }} className="movie__card">
         <Card.Img variant="top" src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
-        <Card.Body className="movie__card--body">
-          <Card.Title className="movie__card--title">{movie.title}</Card.Title>
+        <Card.Body className="p-3 movie__card--body">
+          <Card.Title className="m-0 d-flex justify-content-between align-items-center">
+            <span className="movie__title">{movie.title}</span>
+            <span className="movie__rating">{movie.vote_average}</span>
+          </Card.Title>
         </Card.Body>
       </Card>
     </>
