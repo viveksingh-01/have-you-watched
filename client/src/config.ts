203d0e0d5bdeb6d67a0f0ce3d5907430e4466122
viveksingh-01 <viveksingh9301@gmail.com>
2021-01-17 01:21:06 +0530

    Show popular movies on LandingPage

---
+++ client/src/config.ts
@@ -0,0 +1,1 @@
+export const MOVIE_DB_API_KEY = '6208ea2f09df402b7bf18b6bfbc01ebf';
--- client/src/pages/LandingPage.tsx
+++ client/src/pages/LandingPage.tsx
@@ -1,12 +1,24 @@
-import React, { useState } from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import Catalog from '../components/Catalog';
+import { MOVIE_DB_API_KEY } from '../config';
 
 const LandingPage = () => {
+  const [popularMovies, setPopularMovies] = useState([]);
+
+  useEffect(() => {
+    const fetchPopularMovies = async () => {
+      const { data } = await axios.get(
+        `https://api.themoviedb.org/3/movie/popular?api_key=${MOVIE_DB_API_KEY}&language=en-US`
+      );
+      setPopularMovies(data.results);
+    };
+    fetchPopularMovies();
+  }, []);
+
   return (
     <>
-      <header>
-        <h3>Featured</h3>
-      </header>
-      <section></section>
+      <Catalog header={'Popular Movies'} movies={popularMovies} />
     </>
   );
 };
