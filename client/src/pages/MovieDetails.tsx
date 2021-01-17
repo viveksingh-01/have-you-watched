6bb90a34d5006ca86d269094ecfec44d68cefacb
viveksingh-01 <viveksingh9301@gmail.com>
2021-01-18 01:44:58 +0530

    Show movie-details on MovieDetails page

--- client/src/pages/MovieDetails.tsx
+++ client/src/pages/MovieDetails.tsx
@@ -1,7 +1,8 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
+import { Col, Container, ListGroup, Row } from 'react-bootstrap';
 import { RouteComponentProps } from 'react-router';
-import { API_URL } from '../config/constants';
+import { API_URL, IMAGE_URL } from '../config/constants';
 import { MOVIE_DB_API_KEY } from '../config/key';
 import { IMovieDetail } from '../types/MovieDetail';
 
@@ -26,7 +27,73 @@ const MovieDetails: React.FC<RouteComponentProps<TParams>> = ({ match }) => {
 
   return (
     <>
-      <header>{movieDetail.title}</header>
+      <Container>
+        <Row>
+          <Col>
+            <img src={`${IMAGE_URL}${movieDetail.poster_path}`} alt={movieDetail.title} width="300px" />
+          </Col>
+          <Col>
+            <header className="mb-4">
+              <h3 className="movie-detail__title">{movieDetail.title}</h3>
+              <h6 className="text-muted">
+                <i>"{movieDetail.tagline}"</i>
+              </h6>
+            </header>
+            <section>
+              <h5>Overview</h5>
+              <p>{movieDetail.overview}</p>
+            </section>
+          </Col>
+          <Col>
+            <ListGroup>
+              <ListGroup.Item>
+                <Row>
+                  <Col className="key__col">Rating:</Col>
+                  <Col className="value__col">
+                    {movieDetail.vote_average} ({movieDetail.vote_count} votes)
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col className="key__col">Status:</Col>
+                  <Col className="value__col">{movieDetail.status}</Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col className="key__col">Runtime:</Col>
+                  <Col className="value__col">{movieDetail.runtime} mins</Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col className="key__col">Release Date:</Col>
+                  <Col className="value__col">{movieDetail.release_date}</Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col className="key__col">Popularity:</Col>
+                  <Col className="value__col">{movieDetail.popularity}</Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col className="key__col">Budget:</Col>
+                  <Col className="value__col">${movieDetail.budget}</Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col className="key__col">Revenue:</Col>
+                  <Col className="value__col">${movieDetail.revenue}</Col>
+                </Row>
+              </ListGroup.Item>
+            </ListGroup>
+          </Col>
+        </Row>
+      </Container>
     </>
   );
 };
