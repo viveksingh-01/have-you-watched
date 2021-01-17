936f71cb636d52dfc0565a988462bc58d33a7c04
viveksingh-01 <viveksingh9301@gmail.com>
2021-01-17 12:38:08 +0530

    Add custom loading-spinner

---
+++ client/src/components/LoadingSpinner.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Spinner } from 'react-bootstrap';
+
+const LoadingSpinner = () => {
+  return (
+    <>
+      <Spinner animation="border" role="status">
+        <span className="sr-only">Loading...</span>
+      </Spinner>
+    </>
+  );
+};
+
+export default LoadingSpinner;
