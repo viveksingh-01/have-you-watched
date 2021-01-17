9d6574b0c3945bdfa07fa6ed0a8eb6585fdae921
viveksingh-01 <viveksingh9301@gmail.com>
2021-01-17 11:22:34 +0530

    Modify movie-title font-style

--- client/src/components/MovieCard.tsx
+++ client/src/components/MovieCard.tsx
@@ -9,8 +9,8 @@ const MovieCard = ({ movie }: { movie: Movie }) => {
         <Card.Img variant="top" src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
         <Card.Body className="p-3 movie__card--body">
           <Card.Title className="m-0 d-flex justify-content-between align-items-center">
-            <span className="movie__title">{movie.title}</span>
-            <span className="movie__rating">{movie.vote_average}</span>
+            <span className="mr-2 movie__title">{movie.title}</span>
+            <span className="movie__rating">{movie.vote_average.toFixed(1)}</span>
           </Card.Title>
         </Card.Body>
       </Card>
