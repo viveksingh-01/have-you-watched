979b312a9a6211a834d539283fca9157318dc6b1
viveksingh-01 <viveksingh9301@gmail.com>
2021-01-17 11:22:02 +0530

    Change footer styles

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
   return (
     <>
-      <footer className="py-1 footer">&copy; {new Date().getFullYear()} &nbsp;HaveYouWatched?</footer>
+      <footer>&copy; {new Date().getFullYear()} &nbsp;HaveYouWatched?</footer>
     </>
   );
 };
