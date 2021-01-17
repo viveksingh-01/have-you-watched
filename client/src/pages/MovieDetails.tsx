1575850ba0093a6eaf6def48e1a6b68d2f7da62a
viveksingh-01 <viveksingh9301@gmail.com>
2021-01-18 02:09:54 +0530

    Add rating section in MovieDetails page

--- client/src/pages/MovieDetails.tsx
+++ client/src/pages/MovieDetails.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
-import { Col, Container, ListGroup, Row } from 'react-bootstrap';
+import { Col, ListGroup, Row } from 'react-bootstrap';
 import { RouteComponentProps } from 'react-router';
 import { API_URL, IMAGE_URL } from '../config/constants';
 import { MOVIE_DB_API_KEY } from '../config/key';
@@ -25,75 +25,85 @@ const MovieDetails: React.FC<RouteComponentProps<TParams>> = ({ match }) => {
     fetchMovieDetails();
   }, [match.params.movieId]);
 
+  const getRatingColor = (rating: number | any): string => {
+    let ratingColor = '#ff0000';
+    if (rating >= 8) {
+      ratingColor = '#00ff00';
+    } else if (rating >= 7) {
+      ratingColor = 'yellow';
+    } else if (rating >= 6) {
+      ratingColor = 'orange';
+    }
+    return ratingColor;
+  };
+
   return (
     <>
-      <Container>
-        <Row>
-          <Col>
-            <img src={`${IMAGE_URL}${movieDetail.poster_path}`} alt={movieDetail.title} width="300px" />
-          </Col>
-          <Col>
-            <header className="mb-4">
-              <h3 className="movie-detail__title">{movieDetail.title}</h3>
-              <h6 className="text-muted">
-                <i>"{movieDetail.tagline}"</i>
-              </h6>
-            </header>
-            <section>
-              <h5>Overview</h5>
-              <p>{movieDetail.overview}</p>
-            </section>
-          </Col>
-          <Col>
-            <ListGroup>
-              <ListGroup.Item>
-                <Row>
-                  <Col className="key__col">Rating:</Col>
-                  <Col className="value__col">
-                    {movieDetail.vote_average} ({movieDetail.vote_count} votes)
-                  </Col>
-                </Row>
-              </ListGroup.Item>
-              <ListGroup.Item>
-                <Row>
-                  <Col className="key__col">Status:</Col>
-                  <Col className="value__col">{movieDetail.status}</Col>
-                </Row>
-              </ListGroup.Item>
-              <ListGroup.Item>
-                <Row>
-                  <Col className="key__col">Runtime:</Col>
-                  <Col className="value__col">{movieDetail.runtime} mins</Col>
-                </Row>
-              </ListGroup.Item>
-              <ListGroup.Item>
-                <Row>
-                  <Col className="key__col">Release Date:</Col>
-                  <Col className="value__col">{movieDetail.release_date}</Col>
-                </Row>
-              </ListGroup.Item>
-              <ListGroup.Item>
-                <Row>
-                  <Col className="key__col">Popularity:</Col>
-                  <Col className="value__col">{movieDetail.popularity}</Col>
-                </Row>
-              </ListGroup.Item>
-              <ListGroup.Item>
-                <Row>
-                  <Col className="key__col">Budget:</Col>
-                  <Col className="value__col">${movieDetail.budget}</Col>
-                </Row>
-              </ListGroup.Item>
-              <ListGroup.Item>
-                <Row>
-                  <Col className="key__col">Revenue:</Col>
-                  <Col className="value__col">${movieDetail.revenue}</Col>
-                </Row>
-              </ListGroup.Item>
-            </ListGroup>
-          </Col>
-        </Row>
-      </Container>
+      <Row>
+        <Col>
+          <img src={`${IMAGE_URL}${movieDetail.poster_path}`} alt={movieDetail.title} width="300px" />
+        </Col>
+        <Col>
+          <header className="mb-4">
+            <h3 className="movie-detail__title">{movieDetail.title}</h3>
+            <h6 className="text-muted">
+              <i>"{movieDetail.tagline}"</i>
+            </h6>
+          </header>
+          <section className="mb-4">
+            <h5>Overview</h5>
+            <p>{movieDetail.overview}</p>
+          </section>
+          <section className="mb-4 d-flex align-items-center">
+            <span
+              className="movie-detail__rating mr-3"
+              style={{ backgroundColor: getRatingColor(movieDetail.vote_average) }}>
+              {movieDetail.vote_average?.toFixed(1)}
+            </span>
+            <span className="movie-detail__vote-count">({movieDetail.vote_count} votes)</span>
+          </section>
+        </Col>
+        <Col>
+          <ListGroup>
+            <ListGroup.Item>
+              <Row>
+                <Col className="key__col">Status:</Col>
+                <Col className="value__col">{movieDetail.status}</Col>
+              </Row>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Row>
+                <Col className="key__col">Runtime:</Col>
+                <Col className="value__col">{movieDetail.runtime} mins</Col>
+              </Row>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Row>
+                <Col className="key__col">Release Date:</Col>
+                <Col className="value__col">{movieDetail.release_date}</Col>
+              </Row>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Row>
+                <Col className="key__col">Popularity:</Col>
+                <Col className="value__col">{movieDetail.popularity}</Col>
+              </Row>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Row>
+                <Col className="key__col">Budget:</Col>
+                <Col className="value__col">${movieDetail.budget}</Col>
+              </Row>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Row>
+                <Col className="key__col">Revenue:</Col>
+                <Col className="value__col">${movieDetail.revenue}</Col>
+              </Row>
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+      </Row>
     </>
   );
 };
