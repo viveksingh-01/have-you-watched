02a3ac27798452fbbcd5e55aa366d22ab129f0a5
viveksingh-01 <viveksingh9301@gmail.com>
2021-06-26 00:31:28 +0530

    Remove nav-links from navbar

--- client/src/components/NavigationBar.tsx
+++ client/src/components/NavigationBar.tsx
@@ -1,7 +1,5 @@
-import { faBookmark, faSignInAlt } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
-import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import { Container, Navbar } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 
 const NavigationBar = () => {
@@ -13,25 +11,6 @@ const NavigationBar = () => {
             <Navbar.Brand className="navbar--brand">Have You Watched?</Navbar.Brand>
           </LinkContainer>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="mr-auto">
-              <LinkContainer to="/" exact>
-                <Nav.Link>Home</Nav.Link>
-              </LinkContainer>
-            </Nav>
-            <Nav className="ml-auto">
-              <LinkContainer to="/movies/wishlist">
-                <Nav.Link>
-                  <FontAwesomeIcon icon={faBookmark} className="mr-1" /> Wishlist
-                </Nav.Link>
-              </LinkContainer>
-              <LinkContainer to="/login">
-                <Nav.Link>
-                  <FontAwesomeIcon icon={faSignInAlt} className="mr-1" /> Login
-                </Nav.Link>
-              </LinkContainer>
-            </Nav>
-          </Navbar.Collapse>
         </Container>
       </Navbar>
     </>
